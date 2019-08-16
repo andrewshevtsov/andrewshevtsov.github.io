46f12577b109b84743ab4b3ca9675e86aff523c4
andrey shevtsov <a.shevtsov@topof.ru>
2019-08-16 18:18:09 +0300

    update job-parser

---
+++ jobparser/precache-manifest.58b3ac798b19f490cd8be4e71c3db80a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a3665ddbe222693bd4f0497fc3afb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d2470a033725c5830e6",
+    "url": "/static/css/main.edab1b30.chunk.css"
+  },
+  {
+    "revision": "40fc86e9fcf872fb35a8",
+    "url": "/static/js/2.df3705e4.chunk.js"
+  },
+  {
+    "revision": "0d2470a033725c5830e6",
+    "url": "/static/js/main.da1ddd80.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
